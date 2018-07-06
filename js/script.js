de824808dd36a0025c4f0a2f6f773c70c1b5ed4d
Lulu <kya97@mail.ru>
2018-07-06 15:23:14 +0200

    wasted
    you shouldn't even try

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 var link = document.querySelector(".login-link");
+var popup1 = document.querySelector(".modal-login");
+var close = popup1.querySelector(".modal-close");
 
-var popup = document.querySelector(".modal-login");
-var close = popup.querySelector(".modal-close");
-
-var form = popup.querySelector("form");
-var login = popup.querySelector("[name=login]");
-var password = popup.querySelector("[name=password]");
+var form = popup1.querySelector("form");
+var login = popup1.querySelector(".login-user");
+var password = popup1.querySelector(".login-password");
 
 var isStorageSupport = true;
 var storage = "";
@@ -18,7 +17,7 @@ try {
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
+  popup1.classList.toggle("visually-hidden");
 
   if (storage) {
     login.value = storage;
@@ -30,16 +29,16 @@ link.addEventListener("click", function (evt) {
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal-show");
-  popup.classList.remove("modal-error");
+  popup1.classList.add("visually-hidden");
+  popup1.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
   if (!login.value || !password.value) {
     evt.preventDefault();
-    popup.classList.remove("modal-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
+    popup1.classList.remove("modal-error");
+    popup1.offsetWidth = popup1.offsetWidth;
+    popup1.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
       localStorage.setItem("login", login.value);
@@ -50,9 +49,8 @@ form.addEventListener("submit", function (evt) {
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-      popup.classList.remove("modal-error");
-    }
+    popup1.classList.add("visually-hidden");
+    popup1.classList.remove("modal-error");
   }
 });
+
